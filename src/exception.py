5d1f9a4c413b28b0aa35fddbf5d936654349b14e
= <adanalawneh1@gmail.com>
2025-02-08 02:31:48 +0200

    new files

---
+++ src/exception.py
@@ -0,0 +1,13 @@
+import sys 
+import logging
+def error_masseges(error, error_detail:sys):
+    """Create Custom error masseges"""
+    _,_,exp_tb = error_detail.exc_info()
+    error_massege= f" Error: in file {exp_tb.tb_frame.f_code.co_filename} in line {exp_tb.tb_lineno} error massege {error}"
+    return error_massege
+class CustomException(Exception):
+    def __init__(self, error_massege,error_detail:sys):
+        super().__init__(error_massege)
+        self.error_massege= error_masseges(error_massege, error_detail= error_detail)
+    def __str__(self):
+        return self.error_massege
